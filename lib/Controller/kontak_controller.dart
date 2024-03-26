e842076970cadcbd4a73d0fde6e0209db52eb8ae
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-03-26 19:47:07 +0700

    Add addPerson Method for sending Kontak data to server

--- lib/Controller/kontak_controller.dart
+++ lib/Controller/kontak_controller.dart
@@ -1,5 +1,17 @@
+import 'dart:io';
+
 import 'package:data_kontak/Service/kontak_service.dart';
+import 'package:data_kontak/data_kontak/kontak.dart';
 
 class KontakController {
   final KontakService kontakService = KontakService();
+
+  Future<Map<String, dynamic>> addPerson(Kontak person, File? file) async {
+    Map<String, String> data = {
+      'nama': person.nama,
+      'email': person.email,
+      'alamat': person.alamat,
+      'no_telepon': person.noTelepon,
+    };
+  }
 }
