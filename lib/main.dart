9f631f8f29527cc0503dd9fd1b6dfaa12d34b78c
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-03-30 17:19:40 +0700

    Changes main page to HomeView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:data_kontak/presentation/views/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'presentation/views/registrasi_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: FormKontak(),
+      home: HomeView(),
     );
   }
 }
