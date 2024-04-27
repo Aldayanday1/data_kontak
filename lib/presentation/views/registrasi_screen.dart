3a4700350b0c6d7163ef79d92e298e622740b146
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-04-27 21:10:28 +0700

    Remove widget scaffold

--- lib/presentation/views/registrasi_screen.dart
+++ lib/presentation/views/registrasi_screen.dart
@@ -36,94 +36,87 @@ class _FormKontakState extends State<FormKontak> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Data Kontak"),
-        centerTitle: true,
-        backgroundColor: Color.fromARGB(255, 200, 210, 214),
-      ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(10),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    labelText: "Nama", hintText: "Masukkan Nama"),
-                controller: _namaController,
-              ),
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            child: TextFormField(
+              decoration:
+                  InputDecoration(labelText: "Nama", hintText: "Masukkan Nama"),
+              controller: _namaController,
             ),
-            Container(
-              margin: EdgeInsets.all(10),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    labelText: "Email", hintText: "Masukkan Email"),
-                controller: _emailController,
-              ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  labelText: "Email", hintText: "Masukkan Email"),
+              controller: _emailController,
             ),
-            Container(
-              margin: EdgeInsets.all(10),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    labelText: "Alamat", hintText: "Masukkan Alamat"),
-                controller: _alamatController,
-              ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  labelText: "Alamat", hintText: "Masukkan Alamat"),
+              controller: _alamatController,
             ),
-            Container(
-              margin: EdgeInsets.all(10),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    labelText: "No Telepon", hintText: "Masukkan No Telepon"),
-                controller: _noTeleponController,
-              ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  labelText: "No Telepon", hintText: "Masukkan No Telepon"),
+              controller: _noTeleponController,
             ),
-            SizedBox(
-              height: 50,
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          _image == null
+              ? const Text("Tidak ada gambar yang dipilih")
+              : Image.file(_image!),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 25, top: 35),
+            child: ElevatedButton(
+              onPressed: () {
+                getImage();
+              },
+              child: Text("Pilih Gambar"),
             ),
-            _image == null
-                ? const Text("Tidak ada gambar yang dipilih")
-                : Image.file(_image!),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 25, top: 35),
-              child: ElevatedButton(
-                onPressed: () {
-                  getImage();
-                },
-                child: Text("Pilih Gambar"),
-              ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            child: ElevatedButton(
+              onPressed: () async {
+                if (_formKey.currentState!.validate()) {
+                  _formKey.currentState!.save();
+                  Kontak _person = Kontak(
+                    nama: _namaController.text,
+                    email: _emailController.text,
+                    alamat: _alamatController.text,
+                    noTelepon: _noTeleponController.text,
+                    foto: _image!.path,
+                  );
+                  // Proses simpan data
+                  var result =
+                      await _personController.addPerson(_person, _image);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text(result['message'])),
+                  );
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomeView()),
+                    (route) => false,
+                  );
+                }
+              },
+              child: const Text("Submit"),
             ),
-            Container(
-              margin: EdgeInsets.all(10),
-              child: ElevatedButton(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    Kontak _person = Kontak(
-                      nama: _namaController.text,
-                      email: _emailController.text,
-                      alamat: _alamatController.text,
-                      noTelepon: _noTeleponController.text,
-                      foto: _image!.path,
-                    );
-                    // Proses simpan data
-                    var result =
-                        await _personController.addPerson(_person, _image);
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text(result['message'])),
-                    );
-                    Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(builder: (context) => const HomeView()),
-                      (route) => false,
-                    );
-                  }
-                },
-                child: const Text("Submit"),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
